34d5e2a0a5fb0b1aa0f5952703a49b034bcd103e
atharvaarikar <73347732+atharvaarikar@users.noreply.github.com>
2021-05-09 21:06:31 +0530

    Add files via upload

---
+++ rope.js
@@ -0,0 +1,34 @@
+class rope{
+    constructor(body1,point2){
+
+        var poss={
+            bodyA:body1,
+            pointB:point2,
+            length:60,
+            stiffness:0.2
+        }
+
+        this.a=Matter.Constraint.create(poss);
+        World.add(world,this.a);
+        this.po=point2;
+
+    }
+
+    display(){
+
+        if(this.a.bodyA){
+
+            var pos=this.a.bodyA.position;
+
+            line(pos.x,pos.y,this.po.x,this.po.y);
+    
+        }  
+    }
+
+    fly(){
+        this.a.bodyA=null;
+    }
+        
+    
+}
+
